b94bf9caf6eca4215ed38c92eace675ae4cbb9bd
samuel <zhangleven@gmail.com>
2020-05-11 18:14:14 +0800

    Fix isLogin

--- src/main/java/com/github/entity/AuthResult.java
+++ src/main/java/com/github/entity/AuthResult.java
@@ -11,7 +11,7 @@ public class AuthResult extends Result<User> {
         this.avatar = isLogin ? user.getAvatar() : "";
     }
 
-    public boolean isLogin() {
+    public boolean getIsLogin() {
         return isLogin;
     }
 
